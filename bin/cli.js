d4a5bcda9c0163dcf78d50603132eecd6364c751
receiptline <receiptline@gmail.com>
2025-01-11 01:10:21 +0900

    Added cash drawer status

--- bin/cli.js
+++ bin/cli.js
@@ -30,7 +30,9 @@ const receiptio = require('receiptio');
         'error': 103,
         'offline': 104,
         'disconnect': 105,
-        'timeout': 106
+        'timeout': 106,
+        'drawerclosed': 200,
+        'draweropen': 201
     };
     // print result
     let result = '';
@@ -47,17 +49,17 @@ const receiptio = require('receiptio');
     const params = {
         h: false, // show help
         d: '', // ip address or serial/usb port of target printer
-        o: '', // file to output (if -d option is not found)
-        q: false // check printer status without printing
+        o: '', // file to output (if -d option is not present)
+        q: '' // inquire status (printer/drawer/drawer2)
     };
     // parse arguments
     for (let i = 0; i < argv.length; i++) {
         const key = argv[i];
-        if (/^-[hquvsni]$/.test(key)) {
+        if (/^-[huvsni]$/.test(key)) {
             // option without value
             params[key[1]] = true;
         }
-        else if (/^-[dopcrmbgtl]$/.test(key)) {
+        else if (/^-[dopqcrmbgtl]$/.test(key)) {
             // option with value
             if (i < argv.length - 1) {
                 const value = argv[i + 1];
@@ -65,6 +67,11 @@ const receiptio = require('receiptio');
                     params[key[1]] = value;
                     i++;
                 }
+            }
+            // default value of status inquiry
+            if (key[1] === 'q') {
+                let q = params.q.toLowerCase();
+                params.q = /^drawer2?$/.test(q) ? q : 'printer';
             }
         }
         else if (/^[^-]/.test(key)) {
@@ -82,23 +89,23 @@ usage: receiptio [options] [source]
 source:
   receipt markdown text file
   https://receiptline.github.io/designer/
-  if source is not found, standard input
+  if source is not present, standard input
 options:
   -h                show help
   -d <destination>  ip address or serial/usb port of target printer
-  -o <outfile>      file to output (if -d option is not found)
-                    if -d and -o are not found, standard output
+  -o <outfile>      file to output (if -d option is not present)
+                    if -d and -o are not present, standard output
   -p <printer>      printer control language
                     (default: escpos if -d option is found, svg otherwise)
-                    (escpos, epson, sii, citizen, fit, impact, impactb, generic,
-                     star, starline, emustarline, stargraphic,
-                     starimpact, starimpact2, starimpact3, svg, png, txt, text)
+                    (escpos/epson/sii/citizen/fit/impact/impactb/generic/
+                     star/starline/emustarline/stargraphic/
+                     starimpact/starimpact2/starimpact3/svg/png/txt/text)
                     (png requires puppeteer or sharp)
-  -q                check printer status without printing
+  -q [<device>]     inquire status (printer/drawer/drawer2) (default: printer)
   -c <chars>        characters per line (24-96) (default: 48)
   -u                upside down
-  -v                landscape orientation (for escpos, epson, sii, citizen, star)
-  -r <dpi>          print resolution for -v (180, 203) (default: 203)
+  -v                landscape orientation (for escpos/epson/sii/citizen/star)
+  -r <dpi>          print resolution for -v (180/203) (default: 203)
   -s                paper saving (reduce line spacing)
   -n                no paper cut
   -m [<l>][,<r>]    print margin (left: 0-24, right: 0-24) (default: 0,0)
@@ -107,19 +114,20 @@ options:
   -g <gamma>        image gamma correction (0.1-10.0) (default: 1.0)
   -t <timeout>      print timeout (0-3600 sec) (default: 300)
   -l <language>     language of source file (default: system locale)
-                    (en, fr, de, es, po, it, ru, ja, ko, zh-hans, zh-hant, th, ...)
+                    (en/fr/de/es/po/it/ru/ja/ko/zh-hans/zh-hant/th/...)
 print results:
   success(0), online(100), coveropen(101), paperempty(102),
-  error(103), offline(104), disconnect(105), timeout(106)
+  error(103), offline(104), disconnect(105), timeout(106),
+  drawerclosed(200), draweropen(201)
 examples:
-  receiptio -d COM1 receiptmd.receipt
-  receiptio -d /dev/usb/lp0 receiptmd.receipt
-  receiptio -d /dev/ttyS0 -u -b 160 receiptmd.receipt
-  receiptio -d 192.168.192.168 -p escpos -c 42 receiptmd.receipt
+  receiptio -d COM1 example.receipt
+  receiptio -d /dev/usb/lp0 example.receipt
+  receiptio -d /dev/ttyS0 -u -b 160 example.receipt
+  receiptio -d 192.168.192.168 -p escpos -c 42 example.receipt
   receiptio -d com9 -p impact -q
-  receiptio receiptmd.receipt -o receipt.svg
-  receiptio receiptmd.receipt -p escpos -i -b 128 -g 1.8 -o receipt.prn
-  receiptio < receiptmd.receipt -p png > receipt.png
+  receiptio example.receipt -o receipt.svg
+  receiptio example.receipt -p escpos -i -b 128 -g 1.8 -o receipt.prn
+  receiptio < example.receipt -p png > receipt.png
   echo {c:1234567890} | receiptio | more`);
         process.exitCode = 1;
     }
